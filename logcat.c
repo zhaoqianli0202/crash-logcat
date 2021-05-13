10a627f7d0123130a41e24e2e9b85732383ad44f
Qianli Zhao <zhaoqianli@xiaomi.com>
2021-05-13 21:16:40 +0800

    1. Optimizing the logic of searching BSS region 2. Support logcat type
    use logcat -s/m/r/k to print system/main/radio/kernel log
    default show all logs
    
    Signed-off-by: Qianli Zhao <zhaoqianli@xiaomi.com>

--- logcat.c
+++ logcat.c
@@ -22,14 +22,42 @@ void cmd_logcat(void);     /* Declare the commands and their help data. */
 
 #define PRINT_DEBUGLOG(level, ...) \
 	if (CRASHDEBUG(level))  \
-		fprintf(fp, __VA_ARGS__);  \
+	        fprintf(fp, __VA_ARGS__);
 
 char *log_prio = "UDVDIWEFS";
 #define USER_ADDR_MASK (logcat.user_addr_mask)  //android-r userspace address top 8bit tagged for dynamically detect TBI-compatible devices,prepare for ARM MTE
 #define LOGBUF_ENTRY_OFFSET (logcat.logbuf_entry_offset)
 #define LIST_LAST_ENTRY 0
 #define LIST_NEXT_ENTRY 0x8
-#define LIST_ELENMENT 0x10
+#define LIST_ELENMENT   0x10
+#define VM_READ	        0x00000001
+#define VM_WRITE        0x00000002
+
+typedef enum log_id {
+  LOG_ID_MIN = 0,
+
+  /** The main log buffer. This is the only log buffer available to apps. */
+  LOG_ID_MAIN = 0,
+  /** The radio log buffer. */
+  LOG_ID_RADIO = 1,
+  /** The event log buffer. */
+  LOG_ID_EVENTS = 2,
+  /** The system log buffer. */
+  LOG_ID_SYSTEM = 3,
+  /** The crash log buffer. */
+  LOG_ID_CRASH = 4,
+  /** The statistics log buffer. */
+  LOG_ID_STATS = 5,
+  /** The security log buffer. */
+  LOG_ID_SECURITY = 6,
+  /** The kernel log buffer. */
+  LOG_ID_KERNEL = 7,
+
+  LOG_ID_MAX,
+
+  /** Let the logging function choose the best log target. */
+  LOG_ID_DEFAULT = 0x7FFFFFFF
+} log_id_t;
 
 char *help_logcat[];
 struct log_time {
@@ -52,12 +80,14 @@ struct LogBufferElement{
 
 struct logcat_struct {
         ulong logd_task;
-        ulong end_data;
-        ulong start_brk;
+        ulong vma;
+        ulong vm_start;
+        ulong vm_end;
         ulong brk;
         ulong mmap_base;
 	ulong user_addr_mask; //android-r userspace address top 8bit tagged for dynamically detect TBI-compatible devices
 	ulong logbuf_entry_offset;
+        log_id_t type;
 };
 struct logcat_struct logcat;
 #define LOGD_TASK "logd"
@@ -72,7 +102,10 @@ void __attribute__((constructor))
 logcat_init(void) /* Register the command set. */
 {
         ulong init_task, cur_task, mm_struct, task_list_offset;
+        ulong file, dentry, vm_flags, end_data, start_brk;
+        uint8_t find_vma = 0;
         char task_name[TASK_COMM_LEN];
+        char filename[TASK_COMM_LEN] = {0};
         cur_task = init_task = symbol_value("init_task");
         task_list_offset = MEMBER_OFFSET("task_struct", "tasks");
         do {
@@ -90,15 +123,46 @@ logcat_init(void) /* Register the command set. */
         logcat.logd_task = cur_task;
         if (!readmem(cur_task + MEMBER_OFFSET("task_struct", "mm"), KVADDR, &mm_struct, sizeof(ulong), "mm_struct", RETURN_ON_ERROR))
                 error(FATAL, "Get next task failed\n");
-        if (!readmem(mm_struct + MEMBER_OFFSET("mm_struct", "end_data"), KVADDR, &logcat.end_data, sizeof(ulong), "end_data", RETURN_ON_ERROR))
+        if (!readmem(mm_struct + MEMBER_OFFSET("mm_struct", "end_data"), KVADDR, &end_data, sizeof(ulong), "end_data", RETURN_ON_ERROR))
                 error(FATAL, "Read mm_struct end_data failed\n");
-        if (!readmem(mm_struct + MEMBER_OFFSET("mm_struct", "start_brk"), KVADDR, &logcat.start_brk, sizeof(ulong), "start_brk", RETURN_ON_ERROR))
+        if (!readmem(mm_struct + MEMBER_OFFSET("mm_struct", "start_brk"), KVADDR, &start_brk, sizeof(ulong), "start_brk", RETURN_ON_ERROR))
                 error(FATAL, "Read mm_struct start_brk failed\n");
         if (!readmem(mm_struct + MEMBER_OFFSET("mm_struct", "mmap_base"), KVADDR, &logcat.mmap_base, sizeof(ulong), "mmap_base", RETURN_ON_ERROR))
                 error(FATAL, "Read mm_struct mmap_base failed\n");
         if (!readmem(mm_struct + MEMBER_OFFSET("mm_struct", "brk"), KVADDR, &logcat.brk, sizeof(ulong), "brk", RETURN_ON_ERROR))
                 error(FATAL, "Read mm_struct brk failed\n");
+        if (!readmem(mm_struct + MEMBER_OFFSET("mm_struct", "mmap"), KVADDR, &logcat.vma, sizeof(ulong), "mmap", RETURN_ON_ERROR))
+                error(FATAL, "Read mm_struct mmap failed\n");
 
+	while (logcat.vma) {
+            if (!readmem(logcat.vma + MEMBER_OFFSET("vm_area_struct", "vm_flags"), KVADDR, &vm_flags, sizeof(ulong), "vm_flags", RETURN_ON_ERROR))
+                    error(FATAL, "Read vm_area_struct vm_flags failed\n");
+            if ((vm_flags & VM_READ) && (vm_flags & VM_WRITE)) {  //data&bss must rw property
+                if (!readmem(logcat.vma + MEMBER_OFFSET("vm_area_struct", "vm_start"), KVADDR, &logcat.vm_start, sizeof(ulong), "vm_start", RETURN_ON_ERROR))
+                    error(FATAL, "Read vm_area_struct vm_start  failed\n");
+                if (!readmem(logcat.vma + MEMBER_OFFSET("vm_area_struct", "vm_end"), KVADDR, &logcat.vm_end, sizeof(ulong), "vm_end", RETURN_ON_ERROR))
+                    error(FATAL, "Read vm_area_struct vm_end failed\n");
+                if(logcat.vm_end > end_data && logcat.vm_end <= start_brk) {  //bss must between data and heap,bss maybe a signal vma,maybe merge with data segment.
+                    find_vma = 1;
+                    break;
+                }
+            }
+            //avoid deadloop
+            if (!readmem(logcat.vma + MEMBER_OFFSET("vm_area_struct", "vm_file"), KVADDR, &file, sizeof(ulong), "vm_file", RETURN_ON_ERROR))
+                error(FATAL, "Read mm_struct mmap failed\n");
+            if (file != 0) {
+                if (!readmem(file + MEMBER_OFFSET("file", "f_path") + MEMBER_OFFSET("path", "dentry"), KVADDR, &dentry, sizeof(ulong), "dentry", RETURN_ON_ERROR))
+                    error(FATAL, "Read dentry failed\n");
+                if (!readmem(dentry + MEMBER_OFFSET("dentry", "d_iname"), KVADDR, filename, TASK_COMM_LEN, "filename", RETURN_ON_ERROR))
+                    error(FATAL, "Read dentry d_iname failed\n");
+                if (strncmp(filename, LOGD_TASK, TASK_COMM_LEN))
+                    break;
+            }
+            if (!readmem(logcat.vma + MEMBER_OFFSET("vm_area_struct", "vm_next"), KVADDR, &logcat.vma, sizeof(ulong), "vm_next", RETURN_ON_ERROR))
+                    error(FATAL, "Read vm_area_struct vm_next failed\n");
+        }
+        if(!find_vma)
+            error(FATAL, "Find data/bss section vma failed\n");
         logcat.user_addr_mask = (~((ulong)0xff<<56));
         logcat.logbuf_entry_offset = 0;
         register_extension(command_table);
@@ -115,6 +179,8 @@ int parse_log_entry(ulong log_entry)
         if(readmem(log_entry + LIST_ELENMENT, UVADDR, &log_elenment, sizeof(ulong), "log_elenment", QUIET)) {
 		log_elenment &= USER_ADDR_MASK;
                 if(readmem(log_elenment, UVADDR, &Log, sizeof(struct LogBufferElement), "LogBufferElement", QUIET)) {
+                        if (logcat.type != Log.mLogId && logcat.type != LOG_ID_DEFAULT)
+                          return 0;
                         msg = GETBUF(Log.mMsgLen);
                         nanos = (ulonglong)Log.mRealTime.tv_nsec / (ulonglong)1000000000;
                         rem = (ulonglong)Log.mRealTime.tv_nsec % (ulonglong)1000000000;
@@ -181,13 +247,26 @@ cmd_logcat(void)
         ulong logBuf, addr;
 	int c;
         set_context(logcat.logd_task, NO_PID);
-	while ((c = getopt(argcnt, args, "Q")) != EOF) {
+        logcat.type = LOG_ID_DEFAULT;
+	while ((c = getopt(argcnt, args, "Qsmrk")) != EOF) {
 		switch (c) {
 			case 'Q':
 		                PRINT_DEBUGLOG(INFO, "android-Q logcat\n");
 				logcat.user_addr_mask = (~(ulong)0x0);
 				logcat.logbuf_entry_offset = 8;
 				break;
+                        case 's':
+                                logcat.type = LOG_ID_SYSTEM;
+                                break;
+                        case 'm':
+                                logcat.type = LOG_ID_MAIN;
+                                break;
+                        case 'r':
+                                logcat.type = LOG_ID_RADIO;
+                                break;
+                        case 'k':
+                                logcat.type = LOG_ID_KERNEL;
+                                break;
 			default:
 		                PRINT_DEBUGLOG(INFO, "android-R logcat\n");
 				break;
@@ -197,7 +276,7 @@ cmd_logcat(void)
                 static LogBuffer* logBuf = nullptr;
                 traversal .bss region to locate logBuf address
         )*/
-        for (addr = logcat.end_data; addr < logcat.start_brk; addr += sizeof(ulong))
+        for (addr = logcat.vm_start; addr < logcat.vm_end; addr += sizeof(ulong))
         {
                 if (readmem(addr, UVADDR, &logBuf, sizeof(ulong), "logBuf", QUIET)) {
 			logBuf &= USER_ADDR_MASK;
@@ -215,7 +294,7 @@ cmd_logcat(void)
                         }
                 }
         }
-        if (addr >= logcat.start_brk)
+        if (addr >= logcat.vm_end)
                 fprintf(fp, "Find logBuf failed\n");
 }
 
@@ -224,6 +303,8 @@ char *help_logcat[] = {
         "parse android logcat from ramdump of a crash extension\n",
         "parse android main logcat",
         "command support Android-R default,use \"logcat -Q\" to support older version",
+        "use \"logcat -s/-m/-r/-k\" to support system/main/radio/kernel log,default show all logs",
         "Any question please contact zhaoqianli@xiaomi.com",
+        "version V3",
         NULL
 };
